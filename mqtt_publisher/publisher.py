1e1f8a477ff52abb5f5391db183e54a7a755d838
Juan Esteban Navarro Camacho <jnavcamacho@gmail.com>
2018-04-03 14:39:00 -0600

    add, new connection control when connection

--- mqtt_publisher/publisher.py
+++ mqtt_publisher/publisher.py
@@ -3,24 +3,7 @@ import paho.mqtt.client as mqtt #import the client1
 import threading #MultiThreading
 class Publisher (threading.Thread):
 
-	#callback on recieved message
-	def on_message(self, client, userdata, message):
-		print('\n')
-		print("message received " ,str(message.payload.decode("utf-8")))
-		print("message topic=",message.topic)
-		print("message qos=",message.qos)
-		print("message retain flag=",message.retain)
-
-	#callback on recieved message    
-	def on_connect(self, client, userdata, flags, rc):
-		if rc == 0:
-			print("Connected to broker")
-			self.Connected = True                #Signal connection
-			print("Subscribing to topic","fonagotouch")
-			self.client.subscribe("fonagotouch") 
-		else:
-			print("Connection failed")
-
+	isConnected = False	
 
 	def __init__(self):
 		#init the thread
@@ -41,21 +24,40 @@ class Publisher (threading.Thread):
 		self.client.on_message=self.on_message
 		self.client.on_connect=self.on_connect   
 		print("connecting to broker")
-		self.client.connect(self.broker_address, port=self.serverPort)
-		self.client.loop_start() #start the loop
-
-	def run(self):
 		try:
-			while self.running:
-				pass
-			print("disconnect...")
-			self.client.disconnect()
-		except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-			print "\nKilling Thread..."
-		 	self.running = False
-		 	self.join() # wait for the thread to finish what it's doing
-		
+			self.client.connect(self.broker_address, port=self.serverPort)
+			self.client.loop_start() #start the loop
+			self.isConnected = True
+		except Exception as error:
+			print("Fail during connection")
+			print (error)
+			self.isConnected = False
+			self.__stopThread()#stop the thread
 
+
+	#callback on recieved message
+	def on_message(self, client, userdata, message):
+		print('\n')
+		print("message received " ,str(message.payload.decode("utf-8")))
+		print("message topic=",message.topic)
+		print("message qos=",message.qos)
+		print("message retain flag=",message.retain)
+
+	#callback on recieved message    
+	def on_connect(self, client, userdata, flags, rc):
+		if rc == 0:
+			print("Connected to broker")
+			self.Connected = True                #Signal connection
+			print("Subscribing to topic","fonagotouch")
+			self.client.subscribe("fonagotouch") 
+		else:
+			print("Connection failed")
+			self.__stopThread()
+			
+
+	def __stopThread(self):
+		print ("\nKilling Thread...")
+		self.running = False
 
 	def publish_data(self,data):
 		self.client.publish("fonagotouch",data)
@@ -63,3 +65,24 @@ class Publisher (threading.Thread):
 	def stopPublishing(self):
 		self.running = False
 
+	def __disconnectPublisher(self):
+		print("disconnect...mqtt")
+		try:
+			self.client.disconnect()
+			print('disconnected...mqtt')
+		except Exception:
+			print("Error disconnecting client")		
+
+	def run(self):
+		try:
+			while self.running:
+				pass
+			self.__disconnectPublisher()
+		except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+			print "\nKilling Thread..."
+		 	self.running = False
+		 	self.join() # wait for the thread to finish what it's doing
+		
+
+
+	
--- mqtt_publisher/test.py
+++ mqtt_publisher/test.py
@@ -5,15 +5,23 @@ from random import randint
 import time 
 import os
 #init the publisher object
-publisher = publisher.Publisher()
-publisher.start()#start the thread
-counter = 0
+
 try:
-	while True:
+	MQTT_publisher = publisher.Publisher()#init the object	
+	connected = MQTT_publisher.isConnected# it is connected?
+	if(connected):
+		print("MQTT publisher is connected")
+	else:
+		print("MQTT publisher is not connected")
+	MQTT_publisher.start()#start the thread
+except Exception:
+	print("MQTT creating the publisher")
+try:
+	while connected:
 		time.sleep(2)
 		diction = {'id' : counter}
 		publisher.publish_data(str(diction))
 		counter = counter +1	
-except(KeyboardInterrupt, SystemExit): 
+except(KeyboardInterrupt, SystemExit,Exception): 
 	publisher.stopPublishing()
 	print("bye")
