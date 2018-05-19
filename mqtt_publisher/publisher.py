e47920edf51e304f3c5882ac462744259c86c048
Gustavo Segura <yo@gustavosegura.com>
2018-05-18 22:24:31 -0600

    [add]	Only prints messages on console while running without -O option [add]	Separate in topic (subscribe) and out topic (publish)

--- mqtt_publisher/publisher.py
+++ mqtt_publisher/publisher.py
@@ -4,7 +4,7 @@ import threading #MultiThreading
 import json # to load the config file.
 class Publisher (threading.Thread):
 
-	def __init__(self, topic, broker_address, broker_port=1883, user="", password=""):
+	def __init__(self, broker_address, broker_port=1883, out_topic="", in_topic="", user="", password=""):
 		#init the thread
 		threading.Thread.__init__(self)
 		self.running = True #setting the thread running to true
@@ -13,68 +13,74 @@ class Publisher (threading.Thread):
 		self.user = user
 		self.password = password
 		self.isConnected = False
-		# print("Is creating a new instance of MQTT publisher")
+		self.out_topic=out_topic
+		self.in_topic=in_topic
 		# init the user
-		self.client = mqtt.Client(topic) #create new instance
-
+		if(in_topic):
+			self.client = mqtt.Client(in_topic) #create new instance
+		else:
+			self.client = mqtt.Client()
 		if(user): # ask if auth is required
 			self.client.username_pw_set(self.user, password=self.password)
 		#attach function to callback
 		self.client.on_message=self.on_message
 		self.client.on_connect=self.on_connect
 
-		print("connecting to broker")
+		self.debug("connecting to broker")
 		try:
 			self.client.connect(self.broker_address, port=self.broker_port)
 			self.client.loop_start() #start the loop
 			self.isConnected = True
+
 		except Exception as error:
-			print("Fail during connection")
-			print (error)
+			self.debug("Fail during connection")
+			self.debug (error)
 			self.isConnected = False
 			self.__stopThread()#stop the thread
 
 
 	#callback on recieved message
 	def on_message(self, client, userdata, message):
-		print('\n')
-		print("message received " ,str(message.payload.decode("utf-8")))
-		print("message topic=",message.topic)
-		print("message qos=",message.qos)
-		print("message retain flag=",message.retain)
+		self.debug('\n')
+		self.debug("message received " ,str(message.payload.decode("utf-8")))
+		self.debug("message topic=",message.topic)
+		self.debug("message qos=",message.qos)
+		self.debug("message retain flag=",message.retain)
 
 	#callback on recieved message
 	def on_connect(self, client, userdata, flags, rc):
 		if rc == 0:
-			print("Connected to broker")
+			self.debug("Connected to broker")
 			self.isConnected = True                #Signal connection
-			print("Subscribing to topic","fonagotouch")
-			self.client.subscribe("fonagotouch")
+			if(self.in_topic):
+				self.debug("Subscribing to topic", self.in_topic)
+				self.client.subscribe(self.in_topic)
 		else:
-			print("Connection failed")
+			self.debug("Connection failed")
 			self.__stopThread()
 
 
 	def __stopThread(self):
-		print ("\nKilling Thread...")
+		self.debug("Killing Thread...")
 		self.running = False
 
 	def publish_data(self,data):
 		try:
-			self.client.publish("fonagotouch",data)
+			self.client.publish(self.out_topic,data)
+			self.debug("Published:",data)
 		except Exception:
-			print("Error publishing")
+			self.debug("Error publishing")
 
 	def stopPublishing(self):
 		self.running = False
 
 	def __disconnectPublisher(self):
-		print("disconnect...mqtt")
+		self.debug("disconnect...mqtt")
 		try:
 			self.client.disconnect()
-			print('disconnected...mqtt')
+			self.debug('disconnected...mqtt')
 		except Exception:
-			print("Error disconnecting client")
+			self.debug("Error disconnecting client")
 
 	def run(self):
 		try:
@@ -82,6 +88,11 @@ class Publisher (threading.Thread):
 				pass
 			self.__disconnectPublisher()
 		except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-			print("\nKilling Thread...")
+			self.debug("\nKilling Thread...")
 			self.running = False
 			self.join() # wait for the thread to finish what it's doing
+	def debug(self, *params):
+		if(__debug__):
+			for param in params:
+				print(param, end=' ')
+			print("")
