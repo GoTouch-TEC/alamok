3b79ffee3ce90bac3d8f72a98851f5e6b11a0c38
Gustavo Segura <yo@gustavosegura.com>
2018-05-27 17:56:34 -0600

    [fix] params order

--- mqtt_publisher/publisher.py
+++ mqtt_publisher/publisher.py
@@ -4,7 +4,7 @@ import json # to load the config file.
 import time
 class Publisher ():
 
-	def __init__(self, broker_address, broker_port=1883, out_topic="", in_topic="", user="", password="", on_publish=None, device_id="" ):
+	def __init__(self, broker_address, broker_port=1883, out_topic="", in_topic="", user="", password="", device_id="",on_publish=None):
 		self.broker_address = broker_address
 		self.broker_port = broker_port
 		self.user = user
