31e3bd5df38732f8a90a7fe465273935b044a654
Gustavo Segura <yo@gustavosegura.com>
2018-05-27 21:25:08 -0600

    [add] Credentials

--- gps_logger/test.py
+++ gps_logger/test.py
@@ -1,7 +1,7 @@
 import gps_logger
 
-mqtt_config = {"broker_address":"iot.eclipse","broker_port":1883,"out_topic":"Alamok_TEST","in_topic":"","user":"","password":"","device_id":"AlamokTest"}
-gps_log= gps_logger.GpsLogger(mqtt_config=mqtt_config,db_filename="test.db",device_id="Alamok",refresh_time=2)
+mqtt_config = {"broker_address":"13.59.16.98","broker_port":1884,"out_topic":"fonagotouch","in_topic":"","user":"","password":"","device_id":"5b00d2360a9eaf3f661c81f8"}
+gps_log= gps_logger.GpsLogger(mqtt_config=mqtt_config,db_filename="test.db",device_id="5b00d2360a9eaf3f661c81f8",refresh_time=2)
 
 gps_log.start()
 try:
