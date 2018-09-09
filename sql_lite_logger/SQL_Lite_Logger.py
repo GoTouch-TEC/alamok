9d25012904e22c18dcd07f48a390b8ff8954df3a
Gustavo Segura <yo@gustavosegura.com>
2018-09-08 21:11:07 -0600

    [add]	return rows as dictionaries [fix]	Table PK

--- sql_lite_logger/SQL_Lite_Logger.py
+++ sql_lite_logger/SQL_Lite_Logger.py
@@ -12,54 +12,57 @@ class SQL_Lite_Logger:
 
 	def __init__(self, filename):
 		self.connection = sqlite3.connect(filename, check_same_thread = False)
+		self.connection.row_factory = lambda c, r: dict([(col[0], r[idx]) for idx, col in enumerate(c.description)]) # return every roy as a dictionaty
 		self.cursor = self.connection.cursor()
-		self.db_locations = "locations"
+		self.locations_table = "locations"
 		self.lock = threading.Lock()
 
 		try:
-			self.cursor.execute('''CREATE TABLE {db} (datestamp text, latitude real, longitude real, speed real, altitude real, message_id int, status int) PRIMARY KEY(datestamp) '''.format(db=self.db_locations))
+			sql = '''CREATE TABLE locations (datestamp text, latitude real, longitude real, speed real, altitude real, message_id int, status int, PRIMARY KEY(datestamp)) '''
+			self.cursor.execute(sql)
 
 		except sqlite3.OperationalError:
 			print("Table already exists so table will not be created")
 			self.move_to_failed()
 
-	#{ 'date': 'datasamp'
+	#{ 'datestamp': 'string'
 	#  'latitude': 'double number'
-	#  'longitue': 'double number'
+	#  'longitude': 'double number'
 	#  'altitude': 'double number'
 	#  'speed'   : 'double number'}
 	def backup(self, data, message_id=0):
 		if (data is not None):
-			sql = 		''' INSERT INTO projects(datestamp, latitude, longitue, speed, altitude, message_id, status)
-						VALUES(?, ?, ?, ?, ?, ?, ?) '''
+			data["status"]=2
+			data["message_id"]=message_id
+			sql = 		''' INSERT INTO locations (datestamp, latitude, longitude, speed, altitude, message_id, status)
+						VALUES(:datestamp, :latitude, :longitude, :speed, :altitude, :message_id, :status) '''
 			self.lock.acquire()
-			self.cursor.execute(sql, (data['date'], data['latitude'], data['longitude'], data['speed'], data['altitude'], message_id, 2) )
+			self.cursor.execute(sql,data)
 			self.connection.commit()
 			self.lock.release()
 		else:
 			self.debug("Param data is null")
 
 	def move_to_successful(self,message_id):
-		sql = ''' UPDATE tasks SET status = ? WHERE message_id = ?'''
+		sql = ''' UPDATE locations SET status = ? WHERE message_id = ?'''
 		self.lock.acquire()
 		self.cursor.execute(sql, (1, message_id))
 		self.connection.commit()
 		self.lock.release()
 
-	def move_to_failed(self, elapsed_time=0):
-		sql = ''' UPDATE tasks SET status = ? WHERE status = ?'''
+	def move_to_failed(self):
+		sql = ''' UPDATE locations SET status = ? WHERE status = ?'''
 		self.lock.acquire()
-		self.cursor.execute(sql, (0, 3))
+		self.cursor.execute(sql, (2, 3))
 		self.connection.commit()
 		self.lock.release()
-
 
 	def close(self):
 		print("Close BackUp Data Base")
 		try:
 			self.connection.close()
 		except RuntimeError:
-			print"Error while closing data base connection")
+			print("Error while closing data base connection")
 
 	def fetch_in_progress(self):
 		return self.fetch(2)
@@ -71,58 +74,22 @@ class SQL_Lite_Logger:
 		return self.fetch(3)
 
 	def fetch(self, status):
+		result = []
 		self.lock.acquire()
 		self.cursor.execute('''SELECT * FROM locations WHERE status=?''', (status,))
 		data = self.cursor.fetchall()
 		self.lock.release()
-		data_item = data_succeded.fetchone()
-		while (data_item is not None):
-			result.append(data_item)
-			data_item = data_succeded.fetchone()
-		return result
+		return data
 
-
-	# def fetch_by_elapsed_time(self, db_name,elapsed_time=0):
-	# 	result = [ ]
-	# 	self.lock.acquire()
-	#
-	# 	data_succeded = self.cursor.execute('''SELECT * FROM {db} WHERE datestamp < datetime('now', '-{seconds} seconds')'''.format(db=db_name, seconds=elapsed_time))
-	#
-	# 	self.lock.release()
-	# 	data_item = data_succeded.fetchone()
-	# 	while (data_item is not None):
-	# 		result.append(data_item)
-	# 		data_item = data_succeded.fetchone()
-	# 	return result
-
-	def clean_successful(self):
-		self.clean(self.db_successful)
-
-	def clean_in_progress(self):
-		self.clean(self.db_in_progress)
-
-	def clean_failed(self):
-		self.clean(self.db_failed)
-
-	def clean(self, db_name):
-		string_query = "DELETE FROM " + db_name
-		try:
-			self.lock.acquire()
-			self.cursor.execute(string_query)
-			self.connection.commit()
-			self.lock.release()
-		except RuntimeError:
-			print("Error during deletion of data base")
-
-	def clean_by_elapsed_time(self, db_name, elapsed_time=0):
-		try:
-			self.lock.acquire()
-			self.cursor.execute('''DELETE FROM {db} WHERE datestamp < datetime('now', '-{seconds} seconds') '''.format(db=db_name, seconds=elapsed_time))
-			self.connection.commit()
-			self.lock.release()
-		except RuntimeError:
-			print("Error during deletion of data base")
-
+	def clear(self):
+			sql = "DROP TABLE locations"
+			try:
+				self.lock.acquire()
+				self.cursor.execute(sql)
+				self.connection.commit()
+				self.lock.release()
+			except RuntimeError:
+				print("Error during deletion of data base")
 	def close_logger(self):
 		try:
 			self.connection.close()
